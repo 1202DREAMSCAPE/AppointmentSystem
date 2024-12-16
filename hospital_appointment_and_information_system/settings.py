b386c905be69715c427c908d96aca95e62f178ae
Seajay <cjmcerezo2021@plm.edu.ph>
2024-12-16 19:49:20 +0800

    update 2

--- hospital_appointment_and_information_system/settings.py
+++ hospital_appointment_and_information_system/settings.py
@@ -141,11 +141,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 =======
 >>>>>>> 099cf20 (chanes)
 import os
-import dj_database_url
-
-DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
-}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
