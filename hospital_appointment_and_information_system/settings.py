6dab8c1cdb5ed97137608f3147dd2fd7b9a45039
Seajay <cjmcerezo2021@plm.edu.ph>
2024-12-16 15:50:14 +0800

    chanes

--- hospital_appointment_and_information_system/settings.py
+++ hospital_appointment_and_information_system/settings.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -137,25 +138,31 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+=======
+>>>>>>> 099cf20 (chanes)
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd&d%!!iph5fs6i8tmm!2=o=p4zi8fuw67@(&vlf3$(olghu#3h'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+<<<<<<< HEAD
 DEBUG = False
+=======
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+# Add your domain to allowed hosts
+ALLOWED_HOSTS = ['digicareph.icu', 'www.digicareph.icu']
+>>>>>>> bf4cc06 (chanes)
 
 ALLOWED_HOSTS = ['www.digicareph.icu', 'digicareph.icu','digicare-production-app-d62564394752.herokuapp.com', '127.0.0.1', 'localhost']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 
 # Application definition
 
@@ -184,7 +191,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'hospital_appointment_and_information_system.urls'
+
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -205,8 +214,6 @@ WSGI_APPLICATION = 'hospital_appointment_and_information_system.wsgi.application
 
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -216,8 +223,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -235,8 +240,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Kolkata'
@@ -249,37 +252,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
-#STATICFILES_DIRS=[STATIC_DIR,]
-
-#MEDIA_ROOT=os.path.join(BASE_DIR,'static')
-
-#for contact us give your gmail id and password
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'hospital20host@gmail.com' # this email will be used to send emails
-EMAIL_HOST_PASSWORD = '20hospital' # host email password required
-# now sign in with your host gmail account in your browser
-# open following link and turn it ON
-# https://myaccount.google.com/lesssecureapps
-# otherwise you will get SMTPAuthenticationError at /contactus
-# this process is required because google blocks apps authentication by default
-EMAIL_RECEIVING_USER = ['sophiaerin1005@gmail.com'] # email on which you will receive messages sent from website
-CRISPY_TEMPLATE_PACK = "bootstrap4"
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
-
-# Directory where collectstatic will gather all static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Additional directories to search for static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'hospital/static'),
 ]
@@ -288,5 +262,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Media files configuration
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Email backend configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'your-email@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'your-email-password')
+EMAIL_RECEIVING_USER = ['siddharth25pandey@gmail.com']
+
+# Crispy Forms
+CRISPY_TEMPLATE_PACK = "bootstrap4"
