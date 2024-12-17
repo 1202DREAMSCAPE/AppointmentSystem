0e27ae2e152945e2fd7da8b8af038679063d5277
Seajay <cjmcerezo2021@plm.edu.ph>
2024-12-17 13:37:51 +0800

    database

--- hospital_appointment_and_information_system/settings.py
+++ hospital_appointment_and_information_system/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,10 +83,9 @@ WSGI_APPLICATION = 'hospital_appointment_and_information_system.wsgi.application
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='postgres://localhost:5432/mydatabase'
+    )
 }
 
 
